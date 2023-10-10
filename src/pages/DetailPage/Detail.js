7b95afcc28ea1f70c6c38815ee0b2dd4d879b419
hee-bin <gmlqls4732@naver.com>
2023-10-10 15:46:10 +0900

    detail-page 화면  크기 조정

--- src/pages/DetailPage/Detail.js
+++ src/pages/DetailPage/Detail.js
@@ -3,16 +3,17 @@ import React from 'react';
 import Header from "../../components/detail/Header/Header"
 import StockInfo from "../../components/detail/StockInfo/StockInfo"
 import TopNav from "../../components/detail/TopNav/TopNav"
+import SideNav from "../../components/detail/SideNav/SideNav"
 import Chart from '../../components/detail/Chart/Chart';
 import MarketTrend from '../../components/detail/MarketTrend/MarketTrend';
 import Finance from '../../components/detail/Finance/Finance';
 import NewsAnnouncements from '../../components/detail/NewsAnnouncements/NewsAnnouncements';
 import Reports from '../../components/detail/Reports/Reports';
-import './Detail.module.css';
+import styles from './Detail.module.css'
 
 function DetailPage() {
     return (
-        <div className="detail-page">
+        <div className={styles['detail-page']}>
             <Header />
             <StockInfo />
             <TopNav />
