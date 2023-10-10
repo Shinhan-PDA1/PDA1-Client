fd3cc9f06604275870477179bb406bfa52ddd8b7
hee-bin <gmlqls4732@naver.com>
2023-10-10 18:31:24 +0900

    nav음영추

--- src/components/detail/SideNav/SideNav.js
+++ src/components/detail/SideNav/SideNav.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './SideNav.module.css';
+
 function SideNav({ scrollToComponent }) {
-    
+    const [topPosition, setTopPosition] = useState('30%');
+    const [isBouncing, setIsBouncing] = useState(false); // isBouncing 변수 추가
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const docHeight = document.documentElement.scrollHeight;
+
+            if (scrollY + windowHeight >= docHeight - 50 && !isBouncing) {
+                setIsBouncing(true);
+                setTimeout(() => setIsBouncing(false), 300);
+            } else {
+                setTopPosition(`${30 + scrollY * 0.1}%`);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [isBouncing]);
+
     return (
-        <div className='container'>
+        <div className={`container ${isBouncing ? 'bounce' : ''}`} style={{ top: topPosition }}>
             <nav className={styles['side-nav']}>
                 <ul>
                     {['시세차트', '매매동향', '재무', '뉴스 및 공시', '증권 레포트'].map((item, index) => (
@@ -21,4 +42,3 @@ function SideNav({ scrollToComponent }) {
 }
 
 export default SideNav;
-
