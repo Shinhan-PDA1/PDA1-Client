20f5dc29f78c48126303974cc6c953464aa006fe
hee-bin <gmlqls4732@naver.com>
2023-10-14 13:11:55 +0900

    news  Report anouncement mock data content add

--- src/components/detail/NewsAnnouncements/NewsAnnouncements.js
+++ src/components/detail/NewsAnnouncements/NewsAnnouncements.js
@@ -1,55 +1,54 @@
 import React, { useState } from 'react';
 import '../../../styles/detail/detailGlobal.css'
-
 import styles from './NewsAnnouncements.module.css';
 import mockAnnouncementsData from '../../../data/detail/mockAnnouncementsData';
 import mockNewsData from '../../../data/detail/mockNewsData';
+
 
 function NewsAnnouncements() {
     const [activeTab, setActiveTab] = useState('news');  // 기본 탭은 'news'
 
     return (
         <div className='container'>
-
-        <div className={styles['news-announcements']}>
-            <div className="component-header">
-                <h2>뉴스 º 공시</h2>
-                <p>소식을 알아보고 미리 대응하세요.</p>
-            </div>
-            <div className={styles.tabs}>
-                <button 
-                    onClick={() => setActiveTab('news')}
-                    className={activeTab === 'news' ? `${styles.tabButton} ${styles.active}` : styles.tabButton}
-                >
-                    뉴스
-                </button>
-                <button 
-                    onClick={() => setActiveTab('announcements')}
-                    className={activeTab === 'announcements' ? `${styles.tabButton} ${styles.active}` : styles.tabButton}
-                >
-                    공시
-                </button>
-            </div>
-            {activeTab === 'news' ? (
-                <div className={styles.news}>
-                    {mockNewsData.map((news, index) => (
-                        <div key={index} className={styles.item}>
-                            <h3 className={styles.title}>{news.title}</h3>
-                            <p className={styles.content}>{news.content}</p>
-                        </div>
-                    ))}
+            <div className={styles['news-announcements']}>
+                <div className="component-header">
+                    <h2>뉴스 º 공시</h2>
+                    <p>소식을 알아보고 미리 대응하세요.</p>
                 </div>
-            ) : (
-                <div className={styles.announcements}>
-                    {mockAnnouncementsData.map((announcement, index) => (
-                        <div key={index} className={styles.item}>
-                            <h3 className={styles.title}>{announcement.title}</h3>
-                            <p className={styles.content}>{announcement.content}</p>
-                        </div>
-                    ))}
+                <div className={styles.tabs}>
+                    <button 
+                        onClick={() => setActiveTab('news')}
+                        className={activeTab === 'news' ? `${styles.tabButton} ${styles.active}` : styles.tabButton}
+                    >
+                        뉴스
+                    </button>
+                    <button 
+                        onClick={() => setActiveTab('announcements')}
+                        className={activeTab === 'announcements' ? `${styles.tabButton} ${styles.active}` : styles.tabButton}
+                    >
+                        공시
+                    </button>
                 </div>
-            )}
-        </div>
+                {activeTab === 'news' ? (
+                    <div className={styles.news}>
+                        {mockNewsData.map((news, index) => (
+                            <div key={index} className={styles.item}>
+                                <h3 className={styles.title}>{news.title}</h3>
+                                <p className={styles.content}>{news.content}</p>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className={styles.announcements}>
+                        {mockAnnouncementsData.map((announcement, index) => (
+                            <div key={index} className={styles.item}>
+                                <h3 className={styles.title}>{announcement.title}</h3>
+                                <p className={styles.content}>{announcement.content}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/data/detail/mockReportsData.js
+++ src/data/detail/mockReportsData.js
@@ -1,11 +1,39 @@
 const mockReportsData = [
     {
-        title: "종목 분석 리포트 1",
-        company: "증권사 A"
+        title: "역대 최대 실적 기록",
+        company: "DS투자증권",
+        date: "2023.05.16",
+        views: 31235
     },
     {
-        title: "종목 분석 리포트 2",
-        company: "증권사 B"
+        title: "어닝 + 밸류 + 성장 모멘텀 겸비",
+        company: "DS투자증권",
+        date: "2023.04.28",
+        views: 10432
+    },
+    {
+        title: "3분기 실적 호전과 자회사를 통한 지속적 성장 기대",
+        company: "대우증권",
+        date: "2009.11.04",
+        views: 4948
+    },
+    {
+        title: "예상에 부합하는 2분기 실적. 하반기에도 매출 성장 이어갈 전망",
+        company: "대우증권",
+        date: "2009.08.06",
+        views: 1332
+    },
+    {
+        title: "2분기 실적 개선과 지속되는 글로벌 성장",
+        company: "대우증권",
+        date: "2009.06.24",
+        views: 1107
+    },
+    {
+        title: "글로벌에서 성장하는 Hidden Champion",
+        company: "대우증권",
+        date: "2009.05.18",
+        views: 1431
     },
     // 추가적인 리포트 데이터...
 ];
