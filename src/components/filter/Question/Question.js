663c940ba6fd3b7da81e2f0759710f136ec4db49
Seoyun0626 <tjdbs0626@gmail.com>
2023-10-14 20:03:19 +0900

    필터링 페이지 수정
    Question1-5 클릭시 변화
    Submit 버튼 누르면 mainpage 이동
    Question5 중복 누르기

--- src/components/filter/Question/Question.js
+++ src/components/filter/Question/Question.js
@@ -1,12 +1,15 @@
 // Question.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Question.module.css';
 import yesimg from '../../../assets/images/filter/yes.png';
 import noimg from '../../../assets/images/filter/no.png';
 
 const Question = ({ onNextQuestion }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   const handleAnswer = (answer) => {
+    setSelectedOption(answer);
     onNextQuestion(answer);
   };
 
@@ -15,11 +18,17 @@ const Question = ({ onNextQuestion }) => {
       <h2 className={styles.questionTitle}>Q1.</h2>
       <h3 className={styles.questionText}>투자 경험이 있나요</h3>
       <div className={styles.answerContainer}>
-        <div className={styles.answerOption} onClick={() => handleAnswer('yes')}>
+        <div
+          className={`${styles.answerOption} ${selectedOption === 'yes' ? styles.selected : ''}`}
+          onClick={() => handleAnswer('yes')}
+        >
           <img src={yesimg} alt="Yes" />
           <p>네</p>
         </div>
-        <div className={styles.answerOption} onClick={() => handleAnswer('no')}>
+        <div
+          className={`${styles.answerOption} ${selectedOption === 'no' ? styles.selected : ''}`}
+          onClick={() => handleAnswer('no')}
+        >
           <img src={noimg} alt="No" />
           <p>아니요</p>
         </div>
