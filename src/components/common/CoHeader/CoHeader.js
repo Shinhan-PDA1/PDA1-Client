9c3c97a0c2a0377684b21f67efafe46461a1a8a9
hee-bin <gmlqls4732@naver.com>
2023-10-15 14:37:53 +0900

    coHeader 검색 ui add

--- src/components/common/CoHeader/CoHeader.js
+++ src/components/common/CoHeader/CoHeader.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import TextLogo from "../../../assets/images/common/TextLogo01.png"
-import ZutopiaLogo from "../../../assets/images/common/zutopiaLogo.png"
+import TextLogo from "../../../assets/images/common/TextLogo01.png";
+import ZutopiaLogo from "../../../assets/images/common/zutopiaLogo.png";
+import searchIcon from '../../../assets/images/common/searchIcon.png';
 import styles from './CoHeader.module.css';
+
 
 function Header() {
     const fanID = "Fan ID"; // Replace with the actual ID or name from your state or props
@@ -12,17 +14,22 @@ function Header() {
             <div className={styles.topSection}>
                 <img src={TextLogo} alt="Text Logo" id={styles.TextLogo} />
                 <img src={ZutopiaLogo} alt="Zootopia Logo" id={styles.ZutopiaLogo} />
-
                 <div className={styles.welcomeMessage}>
                     Welcome, {fanID} <span className={styles.editIcon}>✎</span>
                 </div>
             </div>
-            <nav className={styles.mainNav}>
-                <NavLink to="/main" activeClassName={styles.active}>Home</NavLink>
-                <NavLink to="/guide" activeClassName={styles.active}>Jourin's Guide</NavLink>
-                <NavLink to="/detail" activeClassName={styles.active}>Shinhan Report</NavLink>
-                <NavLink to="/filter" activeClassName={styles.active}>My Page</NavLink>
-            </nav>
+            <div className={styles.navContainer}>
+                <nav className={styles.mainNav}>
+                    <NavLink to="/main" activeClassName={styles.active}>Home</NavLink>
+                    <NavLink to="/guide" activeClassName={styles.active}>Jourin's Guide</NavLink>
+                    <NavLink to="/detail" activeClassName={styles.active}>Shinhan Report</NavLink>
+                    <NavLink to="/filter" activeClassName={styles.active}>My Page</NavLink>
+                </nav>
+                <div className={styles.searchContainer}>
+                    <input type="text" placeholder="Search..." className={styles.searchInput} />
+                    <img src={searchIcon} className={styles.searchIcon}></img>
+                </div>
+            </div>
         </header>
     );
 }
