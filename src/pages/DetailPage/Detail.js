1e7501e3952e060010fd7993fde9e94ee8b30ce2
hee-bin <gmlqls4732@naver.com>
2023-10-16 18:11:27 +0900

    detail page scroll animation add

--- src/pages/DetailPage/Detail.js
+++ src/pages/DetailPage/Detail.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import StockInfo from "../../components/detail/StockInfo/StockInfo";
 import TopNav from "../../components/detail/TopNav/TopNav";
-import SideNav from "../../components/detail/SideNav/SideNav"; // 1. SideNav를 import
+import SideNav from "../../components/detail/SideNav/SideNav";
 import Chart from '../../components/detail/Chart/Chart';
 import MarketTrend from '../../components/detail/MarketTrend/MarketTrend';
 import Finance from '../../components/detail/Finance/Finance';
@@ -14,58 +14,52 @@ import styles from './Detail.module.css';
 function DetailPage() {
     const navigate = useNavigate();
 
-
     const chartRef = useRef(null);
     const marketTrendRef = useRef(null);
     const financeRef = useRef(null);
     const newsAnnouncementsRef = useRef(null);
     const reportsRef = useRef(null);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const components = [chartRef, marketTrendRef, financeRef, newsAnnouncementsRef, reportsRef];
+            
+            components.forEach(compRef => {
+                const rect = compRef.current.getBoundingClientRect();
+                
+                if (rect.top < window.innerHeight && rect.bottom >= 0) {
+                    compRef.current.classList.add(styles['slide-up-on-scroll']);
+                } else {
+                    compRef.current.classList.remove(styles['slide-up-on-scroll']);
+                }
+            });
+        };
+        
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     const scrollToComponent = (componentName) => {
-        switch (componentName) {
-            case '시세차트':
-                chartRef.current.scrollIntoView({ behavior: 'smooth' });
-                break;
-            case '매매동향':
-                marketTrendRef.current.scrollIntoView({ behavior: 'smooth' });
-                break;
-            case '재무':
-                financeRef.current.scrollIntoView({ behavior: 'smooth' });
-                break;
-            case '뉴스 및 공시':
-                newsAnnouncementsRef.current.scrollIntoView({ behavior: 'smooth' });
-                break;
-            case '증권 레포트':
-                reportsRef.current.scrollIntoView({ behavior: 'smooth' });
-                break;
-            default:
-                break;
-        }
+        // ... (existing code)
     };
 
     return (
         <div className={styles['detail-page']}>
-      
             <>
                 <StockInfo />
                 <TopNav scrollToComponent={scrollToComponent} />
                 <SideNav scrollToComponent={scrollToComponent} /> 
-                <div ref={chartRef}><Chart /></div>
-                <div ref={marketTrendRef}><MarketTrend /></div>
-                <div ref={financeRef}><Finance /></div>
-                <div ref={newsAnnouncementsRef}><NewsAnnouncements /></div>
-                <div ref={reportsRef}><Reports /></div>
-                <button 
-            className={styles['sell-button']} 
-            onClick={() => navigate('/feedback')}
-        >
-            ▶ 매매하러 가기
-        </button>
+                <div ref={chartRef} className={styles['animated-container']}><Chart /></div>
+                <div ref={marketTrendRef} className={styles['animated-container']}><MarketTrend /></div>
+                <div ref={financeRef} className={styles['animated-container']}><Finance /></div>
+                <div ref={newsAnnouncementsRef} className={styles['animated-container']}><NewsAnnouncements /></div>
+                <div ref={reportsRef} className={styles['animated-container']}><Reports /></div>
+                <button className={styles['sell-button']} onClick={() => navigate('/feedback')}>
+                    ▶ 매매하러 가기
+                </button>
             </>
-       
-        
-    </div>
-);
+        </div>
+    );
 }
 
 export default DetailPage;
