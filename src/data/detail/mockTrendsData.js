3772416a48758f9e02af3ea98b8d1ad8be7fdc53
hee-bin <gmlqls4732@naver.com>
2023-10-19 20:55:34 +0900

    mockTrend data update

--- src/data/detail/mockTrendsData.js
+++ src/data/detail/mockTrendsData.js
@@ -1,6 +1,6 @@
 const mockTrendsData = [
     {
-        date: "10.06",
+        date: "10.19",
         foreignHolding: "+159,243",
         foreignRate: "9.32%",
         institution: "+3,45",
@@ -8,69 +8,139 @@ const mockTrendsData = [
         closingPrice: "55,900",
         difference: "▲3,500",
         volume: "3,999,498"
+    },
+    {
+        date: "10.18",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.17",
+        foreignHolding: "+159,243",
+        foreignRate: "9.32%",
+        institution: "+3,45",
+        individual: "-163,302",
+        closingPrice: "55,900",
+        difference: "▲3,500",
+        volume: "3,999,498"
+    },
+    {
+        date: "10.16",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.15",
+        foreignHolding: "+159,243",
+        foreignRate: "9.32%",
+        institution: "+3,45",
+        individual: "-163,302",
+        closingPrice: "55,900",
+        difference: "▲3,500",
+        volume: "3,999,498"
+    },
+    {
+        date: "10.14",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.13",
+        foreignHolding: "+159,243",
+        foreignRate: "9.32%",
+        institution: "+3,45",
+        individual: "-163,302",
+        closingPrice: "55,900",
+        difference: "▲3,500",
+        volume: "3,999,498"
+    },
+    {
+        date: "10.12",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.11",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.10",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.09",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.08",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.07",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
+    },
+    {
+        date: "10.06",
+        foreignHolding: "+9,243",
+        foreignRate: "3.2%",
+        institution: "+123",
+        individual: "-13,302",
+        closingPrice: "55,400",
+        difference: "▼1,500",
+        volume: "4,999,498"
     },
     {
         date: "10.05",
-        foreignHolding: "+9,243",
-        foreignRate: "3.2%",
-        institution: "+123",
-        individual: "-13,302",
-        closingPrice: "55,400",
-        difference: "▼1,500",
-        volume: "4,999,498"
-    },
-    {
-        date: "10.04",
-        foreignHolding: "+159,243",
-        foreignRate: "9.32%",
-        institution: "+3,45",
-        individual: "-163,302",
-        closingPrice: "55,900",
-        difference: "▲3,500",
-        volume: "3,999,498"
-    },
-    {
-        date: "10.03",
-        foreignHolding: "+9,243",
-        foreignRate: "3.2%",
-        institution: "+123",
-        individual: "-13,302",
-        closingPrice: "55,400",
-        difference: "▼1,500",
-        volume: "4,999,498"
-    },
-    {
-        date: "10.02",
-        foreignHolding: "+159,243",
-        foreignRate: "9.32%",
-        institution: "+3,45",
-        individual: "-163,302",
-        closingPrice: "55,900",
-        difference: "▲3,500",
-        volume: "3,999,498"
-    },
-    {
-        date: "10.01",
-        foreignHolding: "+9,243",
-        foreignRate: "3.2%",
-        institution: "+123",
-        individual: "-13,302",
-        closingPrice: "55,400",
-        difference: "▼1,500",
-        volume: "4,999,498"
-    },
-    {
-        date: "09.30",
-        foreignHolding: "+159,243",
-        foreignRate: "9.32%",
-        institution: "+3,45",
-        individual: "-163,302",
-        closingPrice: "55,900",
-        difference: "▲3,500",
-        volume: "3,999,498"
-    },
-    {
-        date: "09.29",
         foreignHolding: "+9,243",
         foreignRate: "3.2%",
         institution: "+123",
